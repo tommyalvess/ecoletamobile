d0b939a5b5dbf96f0a21127ad7aef923c542eaea
Tom Alves <eliton.alves_santos@hotmail.com>
2021-10-10 17:24:02 -0300

    Final project

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import {Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import  {AppLoading} from 'expo';
+import Routes from './src/routes';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading  />
+  } 
+  
+
+  return (
+      <>
+        <StatusBar barStyle="dark-content" backgroundColor="transparent" />
+        <Routes />
+      </>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
